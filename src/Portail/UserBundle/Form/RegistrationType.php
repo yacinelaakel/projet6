ff4189f99a29fc155450d1842fe8a1f4599b8ac3
Jonathan <cassara.jonathan@gmail.com>
2017-01-19 08:34:37 +0100

    Design 1. De la page inscription

--- src/Portail/UserBundle/Form/RegistrationType.php
+++ src/Portail/UserBundle/Form/RegistrationType.php
@@ -8,9 +8,12 @@
 namespace Portail\UserBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class RegistrationType extends AbstractType
@@ -19,10 +22,8 @@ class RegistrationType extends AbstractType
     {
 
         $builder
-            ->add('Prenom', TextType::class)
-            ->add('Nom', TextType::class);
 
-
+            ;
     }
 
     public function getParent()
