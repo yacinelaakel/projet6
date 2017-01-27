c319b4762c1c23806df9147949fa725569926dee
Jonathan <cassara.jonathan@gmail.com>
2017-01-27 23:35:07 +0100

    UserBundle Design de Inscription et de Connexion

--- src/Portail/UserBundle/Controller/InscriptionController.php
+++ src/Portail/UserBundle/Controller/InscriptionController.php
@@ -8,35 +8,88 @@
 namespace Portail\UserBundle\Controller;
 
 use Portail\UserBundle\Form\RegistrationType;
+use FOS\UserBundle\Event\FilterUserResponseEvent;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\Form\Factory\FactoryInterface;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Model\UserInterface;
+use FOS\UserBundle\Model\UserManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Portail\UserBundle\Controller\Inscription;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-// Contrôleur pour l inscription des membres
+/**
+ * Controller managing the registration.
+ *
+ * @author Thibault Duplessis <thibault.duplessis@gmail.com>
+ * @author Christophe Coevoet <stof@notk.org>
+ */
 class InscriptionController extends Controller
 {
     /**
-     * @return \Symfony\Component\HttpFoundation\Response
-     * @Route("/inscription", name="Inscription")
-     * @Template()
+     * @param Request $request
      *
+     * @return Response
      */
-    public function InscriptionAction()
+    public function InscriptionAction(Request $request)
     {
-        return $this->render('PortailUserBundle:User:inscription.html.twig');
+        /** @var $formFactory FactoryInterface */
+        $formFactory = $this->get('fos_user.registration.form.factory');
+        /** @var $userManager UserManagerInterface */
+        $userManager = $this->get('fos_user.user_manager');
+        /** @var $dispatcher EventDispatcherInterface */
+        $dispatcher = $this->get('event_dispatcher');
+
+        $user = $userManager->createUser();
+        $user->setEnabled(true);
+
+        $event = new GetResponseUserEvent($user, $request);
+        $dispatcher->dispatch(FOSUserEvents::REGISTRATION_INITIALIZE, $event);
+
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
+        }
+
+        $form = $formFactory->createForm();
+        $form->setData($user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $event = new FormEvent($form, $request);
+                $dispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
+
+                $userManager->updateUser($user);
+
+                if (null === $response = $event->getResponse()) {
+                    $url = $this->generateUrl('fos_user_registration_confirmed');
+                    $response = new RedirectResponse($url);
+                }
+
+                $dispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
+
+                return $response;
+            }
+
+            $event = new FormEvent($form, $request);
+            $dispatcher->dispatch(FOSUserEvents::REGISTRATION_FAILURE, $event);
+
+            if (null !== $response = $event->getResponse()) {
+                return $response;
+            }
+        }
+
+        return $this->render('PortailUserBundle:User:inscription.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 
-    public function RegistrationAction(Request $request)
-    {
 
-        $form = $this->createForm(new RegistrationType());
-        // do your code
-
-        return array(
-            'PortailUserBundle:User:inscription.html.twig',
-            array('form' => $form->createView()));
-
-    }
 }
