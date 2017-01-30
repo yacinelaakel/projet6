d9829af19f2ea34eb9fafec87c9a3f18a5cf73f0
jonathan <cassara.jonathan@gmail.com>
2017-01-30 19:44:28 +0100

    Vue Profile Teste Okay

---
+++ src/Portail/UserBundle/Controller/ProfileController.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jonathan
+ * Date: 30/01/2017
+ * Time: 18:42
+ */
+
+
+namespace Portail\UserBundle\Controller;
+
+use FOS\UserBundle\Event\FilterUserResponseEvent;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\Form\Factory\FactoryInterface;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Model\UserInterface;
+use FOS\UserBundle\Model\UserManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+/**
+ * Controller managing the user profile.
+ *
+ * @author Christophe Coevoet <stof@notk.org>
+ */
+class ProfileController extends Controller
+{
+    /**
+     * Show the user.
+     */
+    public function showAction()
+    {
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        return $this->render('PortailUserBundle:Profile:show.html.twig', array(
+            'user' => $user,
+        ));
+    }
+
+}
